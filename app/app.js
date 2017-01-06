d5a26dd89e2b70ddae4928dc3b480b5350fb6f2e
Shahid Khan <shahidahmads1@gmail.com>
2017-01-05 19:43:34 -0500

    Port config

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
-var reload = require('reload');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
 var http = require('http');
 
 
@@ -39,27 +37,6 @@ app.post('/contact', urlencodedParser, function(req, res){
 		statement.finalize();
 	});
 
-	
-	// var connection = mysql.createConnection({
-	// 	host: 'localhost',
-	// 	user: 'root', 
-	// 	password: 'hello', 
-	// 	database: 'sample'
-	// });
-
-	// connection.connect(function(error){
-	// 	if(error){
-	// 		console.log("Error connecting to database");
-	// 	} else {
-	// 		console.log("Successfully connected to database!!");
-	// 	}
-	// });
-
-	// var query = connection.query('insert into contact set ?', contact_details, function(){
-	// 	console.log(query.sql);
-	// });
-
-	// connection.end();
 
 	var transporter = nodemailer.createTransport({
 		service: 'Gmail',
@@ -85,12 +62,9 @@ app.post('/contact', urlencodedParser, function(req, res){
 		} 
 	});
 
-	// $.(document).ready( function(){
-	// 	window.location.href = "/thankyou";
-	// });
-	// res.redirect(307, "/thankyou");
+	
 	console.log("redirecting..");
-	// res.json({"redirect":"http:www.google.com"});
+	
 
 	console.log("redirecting...");
 
@@ -98,7 +72,7 @@ app.post('/contact', urlencodedParser, function(req, res){
 
 });
 
-app.set('port', process.env.PORT | 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
@@ -108,13 +82,14 @@ app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/thankyou'));
-// app.use(require('./routes/resume'));
 
-var server = app.listen(app.get('port'), function(){
+
+var server = app.listen(
+	app.get('port'), function(){
     console.log('Listening on port ' + app.get('port'));
 });
 
-// reload(server, app);
+
 
 
 
